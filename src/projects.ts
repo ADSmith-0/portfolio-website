6c39b1aaba3916bc1a23afa2ff52e4642f063383
ADSmith-0 <adam00199382818@gmail.com>
2024-11-23 02:09:51 +0000

    added projects

---
+++ src/projects.ts
@@ -0,0 +1,59 @@
+import type { Project } from "src/types";
+
+export const projects = [
+  {
+    "id": "grately",
+    "name": "Grately",
+    "projectLink": "",
+    "repoLink": "",
+    "image": {
+      "src": "",
+      "alt": ""
+    },
+    "colour": "pink"
+  },
+  {
+    "id": "chess",
+    "name": "Chess",
+    "projectLink": "",
+    "repoLink": "",
+    "image": {
+      "src": "",
+      "alt": ""
+    },
+    "colour": "pink"
+  },
+  {
+    "id": "dot-files",
+    "name": "Dot Files",
+    "projectLink": "",
+    "repoLink": "",
+    "image": {
+      "src": "",
+      "alt": ""
+    },
+    "colour": "pink"
+  },
+  {
+    "id": "artist-network",
+    "name": "Artist Network",
+    "projectLink": "",
+    "repoLink": "",
+    "image": {
+      "src": "",
+      "alt": ""
+    },
+    "colour": "pink"
+  },
+  {
+    "id": "js-patterns",
+    "name": "JS Patterns",
+    "projectLink": "",
+    "repoLink": "",
+    "image": {
+      "src": "",
+      "alt": ""
+    },
+    "colour": "pink"
+  }
+] as Project[];
