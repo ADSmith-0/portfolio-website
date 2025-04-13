b1bd09d9f3365f9c362ef5b9cd50efc0cca8a62b
ADSmith-0 <adam00199382818@gmail.com>
2025-04-14 00:53:36 +0100

    moved all data into one file

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,95 @@
-import type { Project } from "src/types";
+export type ProjectId =
+  | "grately"
+  | "chess"
+  | "dot-files"
+  | "artist-network"
+  | "js-patterns"
+  | "advent-of-code-go-2021"
+  | "typescript-server"
+  | "c-programming-language"
+  | "portfolio-website"
+  | "traversy-js-challenges";
+
+export type TechnologyId =
+  | "javascript"
+  | "typescript"
+  | "nativescript"
+  | "web-components-api"
+  | "go"
+  | "oauth"
+  | "react"
+  | "react-native"
+  | "svelte"
+  | "sveltekit"
+  | "next"
+  | "astro"
+  | "neovim"
+  | "tmux"
+  | "alacritty"
+  | "c"
+  | "pwa";
+
+export type Version = Pick<
+  Project,
+  "technologies" | "repo" | "about"
+> &
+  Partial<Pick<Project, "link">>;
+
+export type Project = {
+  id: ProjectId;
+  name: string;
+  description: string;
+  about: string;
+  technologies: TechnologyId[];
+  link: string;
+  repo: string;
+  colour: string;
+  versions?: Version[];
+};
+
+export const technologyFromId: Record<
+  TechnologyId,
+  { name: string; link: string }
+> = {
+  go: { name: "Go", link: "https://go.dev/" },
+  astro: { name: "Astro", link: "https://astro.build/" },
+  react: { name: "React", link: "https://react.dev/" },
+  "react-native": { name: "React Native", link: "https://reactnative.dev/" },
+  svelte: { name: "Svelte", link: "https://svelte.dev/" },
+  sveltekit: {
+    name: "SvelteKit",
+    link: "https://svelte.dev/tutorial/kit/introducing-sveltekit",
+  },
+  next: {
+    name: "Next",
+    link: "https://nextjs.org/",
+  },
+  javascript: {
+    name: "JavaScript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  typescript: { name: "TypeScript", link: "https://www.typescriptlang.org/" },
+  "web-components-api": {
+    name: "Web Components API",
+    link: "https://developer.mozilla.org/en-US/docs/Web/API/Web_components",
+  },
+  oauth: { name: "OAuth", link: "https://en.wikipedia.org/wiki/OAuth" },
+  neovim: { name: "Neovim", link: "https://neovim.io/" },
+  tmux: { name: "Tmux", link: "https://github.com/tmux/tmux/wiki" },
+  alacritty: { name: "Alacritty", link: "https://alacritty.org/" },
+  c: {
+    name: "C Programming Language",
+    link: "https://en.wikipedia.org/wiki/C_(programming_language)",
+  },
+  nativescript: {
+    name: "NativeScript",
+    link: "https://nativescript.org/",
+  },
+  pwa: {
+    name: "Progressive Web App",
+    link: "https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/What_is_a_progressive_web_app",
+  },
+};
 
 export const projects: Project[] = [
   {
