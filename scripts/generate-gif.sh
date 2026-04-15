98e63faed1c6a2ccf2c647e19265e6091a2990fa
ADSmith-0 <adam00199382818@gmail.com>
2026-04-15 18:59:07 +0100

    Improved framerate for gifs

--- scripts/generate-gif.sh
+++ scripts/generate-gif.sh
@@ -4,12 +4,16 @@ echo -e "\x1b[1;34mGenerating gif for $1...\x1b[0m"
 
 cd ~/Pictures/Blender_exports/$1/
 
-convert -delay 10 -loop 0 -dispose Background *.png \
+convert \
+  -coalesce \
+  -delay 5 \
+  -loop 0 \
+  -dispose Background *.png \
   -fuzz 2% -transparent "#404040" \
-  -layers optimize \
+  -layers OptimizeTransparency \
   $1.gif
 
-gifsicle -O3 --colors 128 $1.gif --lossy=30 -o $1_optimised.gif
+gifsicle -O3 --colors 128 $1.gif -o $1_optimised.gif
 
 rm $1.gif
 
